30e5385e034eb30ebe1b6b10b6bd782e5643fa32
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-07-10 09:12:09 +0530

    constants and multiline comments; renamed all files in serial order

---
+++ 5_multiline_comments_and_constants.rb
@@ -0,0 +1,12 @@
+=begin
+multiline comments
+weird syntax
+=end
+
+# all capitals is a constant in ruby
+A_CONSTANT = 1.22
+
+# value of the constant gets changed with a warning on log
+A_CONSTANT = 1.33
+
+puts A_CONSTANT
